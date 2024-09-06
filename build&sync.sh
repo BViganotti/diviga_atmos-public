f4fb014357b2e14c92e05067dccfc5f417984f52
bviganotti <bviganotti@magicleap.com>
2024-09-06 12:18:02 +0300

    #

--- build&sync.sh
+++ build&sync.sh
@@ -3,7 +3,7 @@ cargo fmt
 echo "BUILDING"
 cross build --release --target=arm-unknown-linux-gnueabihf
 
-echo "PUSHING"
+#echo "PUSHING"
 # leave this as atmos888 not to mess up the cron job
-scp -o StrictHostKeyChecking=no target/arm-unknown-linux-gnueabihf/release/readwrite naughty@naughty:~/atmos999
-scp -o StrictHostKeyChecking=no dht.py naughty@naughty:~/dht.py
+#scp -o StrictHostKeyChecking=no target/arm-unknown-linux-gnueabihf/release/readwrite naughty@naughty:~/atmos999
+#scp -o StrictHostKeyChecking=no dht.py naughty@naughty:~/dht.py
